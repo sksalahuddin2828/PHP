db9b02eaaf74adacd8ab515aa7fb04b4f4b0664a
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 14:52:34 +0600

    Update Coding_Challenges.php

--- Coding_Challenges.php
+++ Coding_Challenges.php
@@ -70,6 +70,19 @@ for ($i = 0; $i < $n; $i++) {
 
 4. Returning the Nth Value from the Fibonacci Sequence:
 
+<?php
+function fibonacci($n) {
+    if ($n <= 1) {
+        return $n;
+    } else {
+        return fibonacci($n - 1) + fibonacci($n - 2);
+    }
+}
+
+$n = 10;
+$fibonacci_number = fibonacci($n);
+echo $fibonacci_number;
+?>
 
 
 --------------------------------------------------------------------------------------------------------------------------------------------
