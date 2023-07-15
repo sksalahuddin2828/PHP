062c83da6b005998c6cc1d65943e2d30955bc048
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:51:46 +0600

    Update armstrong_number.php

--- armstrong_number.php
+++ armstrong_number.php
@@ -17,3 +17,21 @@ if (is_armstrong_number($user_input)) {
 } else {
     echo "$user_input is not an Armstrong number." . PHP_EOL;
 }
+
+//-------------------------------------------------------------------------------------
+
+function is_armstrong_number($n) {
+    $numDigits = strlen((string)$n);
+    $sumOfPowers = 0;
+    $temp = $n;
+    while ($temp > 0) {
+        $digit = $temp % 10;
+        $sumOfPowers += pow($digit, $numDigits);
+        $temp = (int)($temp / 10);
+    }
+    return $n === $sumOfPowers;
+}
+
+echo is_armstrong_number(153) ? 'true' : 'false';  
+
+// Output: true
