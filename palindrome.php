f92821b9421c781b8f8fb25794aa545f22bdbc47
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:37:27 +0600

    Create palindrome.php

---
+++ palindrome.php
@@ -0,0 +1,12 @@
+function is_palindrome($s) {
+    $s = preg_replace("/[^0-9a-zA-Z]/", "", strtolower($s));
+    return $s === strrev($s);
+}
+
+echo "Enter a string: ";
+$user_input = readline();
+if (is_palindrome($user_input)) {
+    echo "'$user_input' is a palindrome." . PHP_EOL;
+} else {
+    echo "'$user_input' is not a palindrome." . PHP_EOL;
+}
