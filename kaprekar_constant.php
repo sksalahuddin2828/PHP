e37ad80fcc5802809e3ebe2526b3d50ecf08ade1
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:05:52 +0600

    Update kaprekar_constant.php

--- kaprekar_constant.php
+++ kaprekar_constant.php
@@ -14,3 +14,21 @@ echo "Enter a number: ";
 $user_input = readline();
 $steps = kaprekar_constant($user_input);
 echo "Number of steps to reach Kaprekar constant: $steps" . PHP_EOL;
+
+//------------------------------------------------------------------------------
+
+function kaprekar_constant($n) {
+    $count = 0;
+    while ($n != 6174) {
+        $count++;
+        $digits = str_pad($n, 4, '0', STR_PAD_LEFT);
+        $ascending = (int)implode('', array_reverse(str_split($digits)));
+        $descending = (int)implode('', str_split($digits));
+        $n = $descending - $ascending;
+    }
+    return $count;
+}
+
+echo kaprekar_constant(1234);  
+
+// Output: 3
